a950d703a8b5b1c403f95861e9b77e33dc8d48ed
zyusou <zyusou@live.jp>
2017-01-13 02:02:11 +0900

    Fuck'in InputField bug modified

--- Assets/Scripts/InputWord.cs
+++ Assets/Scripts/InputWord.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class InputWord : MonoBehaviour {
 
 	private InputField _inputField;
-	private string accept_char = "アイウエオカキクケコサシスセソタチツテト"
+	private string _acceptChar = "アイウエオカキクケコサシスセソタチツテト"
 	+ "ナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
 	+ "ーガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポァィゥェォッュャョ";
-
+	
 	// Use this for initialization
 	void Start () {
 		_inputField = GetComponent<InputField>();
@@ -22,20 +22,20 @@ public class InputWord : MonoBehaviour {
 
 	public void EndEdit(){
 		if(AssertInput(_inputField)){
-
+			Debug.Log("OK");
 		}
 		else{
-			
+			Debug.Log("NG");
 		}
 	}
 
 	private bool AssertInput(InputField input){
-		foreach(char c in input.text)
-		{
-			if(accept_char.IndexOf(c) == -1){
+		foreach(char c in input.textComponent.text){
+			if(_acceptChar.IndexOf(c) == -1){
 				return false;
 			}
 		}
+
 		return true;
 	}
 }
