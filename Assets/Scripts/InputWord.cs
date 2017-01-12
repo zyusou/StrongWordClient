6513dce58659ac4842e60f747792d6ec52f5635e
zyusou <zyusou@live.jp>
2017-01-13 01:21:50 +0900

    InputField Script add

--- Assets/Scripts/InputWord.cs
+++ Assets/Scripts/InputWord.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputWord : MonoBehaviour {
 
+	private InputField _inputField;
+	private string accept_char = "アイウエオカキクケコサシスセソタチツテト"
+	+ "ナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン"
+	+ "ーガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポァィゥェォッュャョ";
+
 	// Use this for initialization
 	void Start () {
-		
+		_inputField = GetComponent<InputField>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void EndEdit(){
+		if(AssertInput(_inputField)){
+
+		}
+		else{
+			
+		}
+	}
+
+	private bool AssertInput(InputField input){
+		foreach(char c in input.text)
+		{
+			if(accept_char.IndexOf(c) == -1){
+				return false;
+			}
+		}
+		return true;
 	}
 }
